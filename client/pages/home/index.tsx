c3b13accc8b759164105f5f13c340aae2b61cdda
kenichi noshian <nosikenit@gmail.com>
2021-11-29 13:47:20 +0900

    feat: add a autocomplete search feature

--- client/pages/home/index.tsx
+++ client/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import styles from "./index.module.scss";
+import Styles from "./index.module.scss";
 import { useState } from "react";
 import {
   GoogleMap,
@@ -6,13 +6,12 @@ import {
   Marker,
   DirectionsService,
   DirectionsRenderer,
+  Autocomplete,
 } from "@react-google-maps/api";
-import Card from "@mui/material/Card";
-import { Container } from "@mui/material";
+import { TextField, Card } from "@mui/material";
 import Divider from "../../components/design/Divider";
 // iconのインポート
 import { faBurn, faBiking, faClock } from "@fortawesome/free-solid-svg-icons";
-
 // TODO: 経路計測時のQueryLIMITについて調査
 
 const home = () => {
@@ -32,6 +31,15 @@ const home = () => {
   const [response, setResponse] = useState(
     null as google.maps.DirectionsResult
   );
+
+  // autocomplete用
+  const [autocomplete, setAutocomplete] = useState(null);
+  const onPlaceChanged = () => {
+    const positionJson = autocomplete.getPlace().geometry?.location.toJSON();
+    if (positionJson !== undefined) {
+      setCenter({ lat: positionJson.lat, lng: positionJson.lng });
+    }
+  };
 
   const getDirections = (event: any) => {
     event.preventDefault();
@@ -59,6 +67,10 @@ const home = () => {
     }
   };
 
+  const autoCompleteOnChangePlaced = (event) => {
+    console.log(event);
+  };
+
   //マーカー関係
   const [markerPositions, setMakerPositions] = useState(
     [] as Array<google.maps.LatLngLiteral>
@@ -77,65 +89,74 @@ const home = () => {
 
   return (
     <>
-      <Container>
-        <Card className={styles.card}>
-          <div className={styles.map}>
-            <LoadScript googleMapsApiKey="AIzaSyBqEkWJHZ0y1BAAKZMxu1gFT3FojQkaG-o">
-              <GoogleMap
-                options={{ mapId: "cb45e1d3ef965ca5", disableDefaultUI: true }}
-                mapContainerStyle={{
-                  width: "900px",
-                  height: "600px",
-                }}
-                center={center}
-                zoom={8}
-                onLoad={handleOnLoad}
-                onClick={getPosition}
-              >
-                {origin !== null && destination !== null && (
-                  <DirectionsService
-                    options={{
-                      origin: origin,
-                      destination: destination,
-                      travelMode: "WALKING" as google.maps.TravelMode,
-                      waypoints: waypoints,
-                    }}
-                    callback={directionsCallback}
-                  />
-                )}
-                {response !== null && (
-                  <DirectionsRenderer
-                    options={{
-                      directions: response,
-                    }}
-                  />
-                )}
-                {markerPositions.map((position, index) => {
-                  return <Marker position={position} key={index} />;
-                })}
-              </GoogleMap>
-            </LoadScript>
-            <button onClick={getDirections}>経路を求める</button>
-          </div>
-          <div className="dataDisplay">
-            <Divider
-              icon={faBiking}
-              primary="走行距離"
-              secondary={distance / 1000 + "km"}
-            />
-            <Divider
-              icon={faClock}
-              primary="時間"
-              secondary={(distance / 1000 / 25) * 60 + "分"}
-            />
-            <Divider
-              icon={faBurn}
-              primary="消費カロリー"
-              secondary={10 * 65 * (distance / 1000 / 25) * 1.05 + "kcal"}
-            />
-          </div>
-        </Card>
-      </Container>
+      <Card className={Styles.card}>
+        <div className={Styles.map}>
+          <LoadScript
+            googleMapsApiKey="AIzaSyBqEkWJHZ0y1BAAKZMxu1gFT3FojQkaG-o"
+            libraries={["places"]}
+          >
+            <Autocomplete
+              onLoad={(autocomplete) => {
+                console.log(setAutocomplete(autocomplete));
+              }}
+              onPlaceChanged={onPlaceChanged}
+            >
+              <TextField variant="outlined" id={Styles.searchBox} />
+            </Autocomplete>
+            <GoogleMap
+              options={{ mapId: "cb45e1d3ef965ca5", disableDefaultUI: true }}
+              mapContainerStyle={{
+                width: "125vh",
+                height: "90vh",
+              }}
+              center={center}
+              zoom={15}
+              onLoad={handleOnLoad}
+              onClick={getPosition}
+            >
+              {origin !== null && destination !== null && (
+                <DirectionsService
+                  options={{
+                    origin: origin,
+                    destination: destination,
+                    travelMode: "WALKING" as google.maps.TravelMode,
+                    waypoints: waypoints,
+                  }}
+                  callback={directionsCallback}
+                />
+              )}
+              {response !== null && (
+                <DirectionsRenderer
+                  options={{
+                    directions: response,
+                  }}
+                />
+              )}
+              {markerPositions.map((position, index) => {
+                return <Marker position={position} key={index} />;
+              })}
+            </GoogleMap>
+          </LoadScript>
+          <button onClick={getDirections}>経路を求める</button>
+        </div>
+        <div className={Styles.dataDisplay}>
+          <Divider
+            icon={faBiking}
+            primary="走行距離"
+            secondary={distance / 1000 + "km"}
+          />
+          <Divider
+            icon={faClock}
+            primary="時間"
+            secondary={(distance / 1000 / 25) * 60 + "分"}
+          />
+          <Divider
+            icon={faBurn}
+            primary="消費カロリー"
+            secondary={10 * 65 * (distance / 1000 / 25) * 1.05 + "kcal"}
+          />
+        </div>
+      </Card>
     </>
   );
 };
