b0a08548f82936bed0806d078a774219e49aeff1
kenichi noshian <nosikenit@gmail.com>
2022-02-11 12:10:54 +0900

    feat[client]: feat post tweet

--- client/components/Timeline/TweetComponent.tsx
+++ client/components/Timeline/TweetComponent.tsx
@@ -1,40 +1,21 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
 import Avatar from "@mui/material/Avatar";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
-import { Tweet } from "../../types/timelines";
 import Image from "next/Image";
-
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+import { Tweet } from "../../types/timelines";
+import { parseJSON, getDate, getYear, getMonth } from "date-fns";
 
 const TweetComponent = (props: Tweet) => {
   const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+  const date = parseJSON(props.createdAt);
+  const timeString = `${getYear(date)}年${getMonth(date)}月${getDate(date)}日`;
 
   return (
     <Card>
@@ -48,7 +29,7 @@ const TweetComponent = (props: Tweet) => {
           </Avatar>
         }
         title={props.user.username}
-        subheader={props.createdAt}
+        subheader={timeString}
       />
       {/* <CardMedia
         component="img"
@@ -57,11 +38,9 @@ const TweetComponent = (props: Tweet) => {
         alt="Paella dish"
       /> */}
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          {props.content}
-        </Typography>
+        <Typography variant="inherit">{props.content}</Typography>
       </CardContent>
-      <CardActions disableSpacing>
+      <CardActions>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
         </IconButton>
--- client/pages/timeline/index.tsx
+++ client/pages/timeline/index.tsx
@@ -3,9 +3,29 @@ import TweetComponent from "../../components/Timeline/TweetComponent";
 import { useEffect, useState } from "react";
 import axios from "../../axisoApi";
 import { Tweet } from "../../types/timelines";
+import { Avatar, Button, TextField } from "@mui/material";
+import { useSelector } from "react-redux";
+import { UserState } from "../../reducks/user/userSlice";
+import Image from "next/image";
 
 const TimeLine = () => {
+  const store = useSelector((state: UserState) => state);
   const [tweets, setTweets] = useState<Tweet[] | undefined>(undefined);
+  const [content, setContent] = useState<string>("");
+
+  const postTweet = async () => {
+    await axios
+      .post<Tweet>("/timelines/", {
+        content: content,
+        userId: store.user.id,
+      })
+      .then((response) => {
+        console.log(response.data);
+        const tempTweets = [response.data, ...tweets!];
+        setTweets(tempTweets);
+        setContent("");
+      });
+  };
 
   useEffect(() => {
     axios.get<Tweet[]>("/timelines/all").then((response) => {
@@ -17,11 +37,40 @@ const TimeLine = () => {
   return (
     <>
       <div className={Styles.wrapper}>
-        <div className={Styles.profile}>あ</div>
+        <div className={Styles.profile}>
+          <div className={Styles.avatar}>
+            <Image
+              src={`https://ddx5fuyp1f5xu.cloudfront.net/${store.user.userImage}`}
+              layout="fill"
+            />
+          </div>
+          <TextField
+            multiline
+            rows={4}
+            placeholder="投稿を入力"
+            variant="filled"
+            value={content}
+            onChange={(event) => {
+              setContent(event.target.value);
+            }}
+            className={Styles.postForm}
+          />
+          <Button
+            variant="contained"
+            className={Styles.postButton}
+            onClick={() => postTweet()}
+          >
+            投稿する!
+          </Button>
+        </div>
         <div className={Styles.timeline}>
           {tweets !== undefined &&
             tweets.map((tweet) => {
-              return <TweetComponent {...tweet}></TweetComponent>;
+              return (
+                <div className={Styles.tweetCard}>
+                  <TweetComponent {...tweet}></TweetComponent>
+                </div>
+              );
             })}
         </div>
         <div className={Styles.info}>あ</div>
